28c964f0987bb3363e793631aaa884558ece1c38
Andrey Mamedov <mam28@yandex.ru>
2020-04-29 10:48:17 +0800

    Chapter 3.1-3.4

---
+++ ch03/ch.3.4-compare.cpp
@@ -0,0 +1,19 @@
+#include "std_lib_facilities.h"
+
+//-------------------------------------------------------------------------------
+
+// read and compare names:
+int main()
+{
+	cout << "Please enter two names\n";
+	string first;
+	string second;
+	cin >> first >> second;		// read two strings
+	if (first == second) cout << "That's the same name twice\n";
+	if (first < second)
+		cout << first << " is alphabetically before " << second << '\n';
+	if (first > second)
+		cout << first << " is alphabetically after " << second << '\n';
+
+	return 0;
+}
---
+++ ch03/ch.3.4-concat.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+
+//-------------------------------------------------------------------------------
+
+// read first and second name
+int main()
+{
+	cout << "Please enter your first and second names\n";
+	string first;
+	string second;
+	cin >> first >> second;						// read two strings
+	string name = first + ' ' + second;		// concatenate strings
+	cout << "Hello, " << name << '\n';
+
+	return 0;
+}
